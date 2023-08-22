4cb59670ff659116ce23d73606c4247659a29002
Ivo Pascal <ivojavierpr@gmail.com>
2023-08-22 03:35:09 -0300

    Add README.md

---
+++ README.md
@@ -0,0 +1,15 @@
+Este es un proyecto creado con la finalidad de aprender sobre APIs en .NET7
+He tratado de hacer a mano una API de una red social como Twitter, Instagram, etc.
+Está desarrollada mediante el método "Code First" utilizando LINQ.
+
+Para utilizarlo, solo deben clonar el proyecto, hacer un build y asegurarse que
+las dependencias estén en orden. Además, ya sea desde la terminal o desde el mismo Visual Studio, pueden añadir y actualizar
+las migraciones a la base de datos y estaría todo listo!
+
+A fecha de hoy, 22/08/23 sigue en proceso, así que pueden llegar a encontrarse
+problemas.
+
+Cabe recalcar que es completamente abierta y cualquier sugerencia es bienvenida,
+espero que ayude para programadores que, como yo, quieran aprender este lenguaje,
+para ello iré dejando comentarios a lo largo del código explicando distintas porciones
+para que sea lo más entendible posible! <3
---
+++ SocialApi-NET7/Migrations/20230822062136_InitialData.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace SocialApi_NET7.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Username = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    Password = table.Column<string>(type: "text", nullable: false),
+                    CreationDate = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Followers",
+                columns: table => new
+                {
+                    FollowerID = table.Column<int>(type: "integer", nullable: false),
+                    FollowingID = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Followers", x => new { x.FollowerID, x.FollowingID });
+                    table.ForeignKey(
+                        name: "FK_Followers_User_FollowerID",
+                        column: x => x.FollowerID,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Followers_User_FollowingID",
+                        column: x => x.FollowingID,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Posts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Body = table.Column<string>(type: "text", nullable: false),
+                    ImageUrls = table.Column<List<string>>(type: "text[]", nullable: false),
+                    CreationDate = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    Likes = table.Column<int>(type: "integer", nullable: false),
+                    AuthorId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Posts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Posts_User_AuthorId",
+                        column: x => x.AuthorId,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Followers_FollowingID",
+                table: "Followers",
+                column: "FollowingID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Posts_AuthorId",
+                table: "Posts",
+                column: "AuthorId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Followers");
+
+            migrationBuilder.DropTable(
+                name: "Posts");
+
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
